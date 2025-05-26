3fabcef66de4069e20b2cc0471f51a4e6786dffa
cfsen <103598332+cfsen@users.noreply.github.com>
2025-05-26 11:08:39 +0200

    chore: clean up dead code from App.tsx and App.css

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,15 @@
 import { useState, useCallback, useEffect } from "react";
+
 import { invoke } from "@tauri-apps/api/core";
+
 import "./App.css";
 
 import { useModalKey } from './keyboard/KeyboardModule';
-import { CommandSequence } from './keyboard/Command';
-
 import { useCommand } from './context/vimagesCtx';
-import Vimage from "./components/Vimage";
 import VimageGrid from "./components/VimageGrid";
 
 function App() {
 	const { handleCmd } = useCommand();
-	const [cmdLog, setCmdLog] = useState<CommandSequence[]>([]);
 	const [fsPwdEntities, setFsPwdEntities] = useState<string>();
 
 	const fs_list = useCallback( async (path: string) => {
@@ -28,12 +26,10 @@ function App() {
 	useModalKey({
 		onSequenceComplete: (seq) => {
 			handleCmd(seq);
-			setCmdLog(cmdLog => [...cmdLog, seq]);
 		},
 	}
 	);
 
-//{cmdLog.map((cmd, index) => <div key={index}>{cmd.cmd.toString()}</div>)}
 	return (
 		<main className="container">
 			<div className="row">
@@ -42,7 +38,6 @@ function App() {
 			<div className="row">
 				{fsPwdEntities}
 			</div>
-
 		</main>
 	);
 }
