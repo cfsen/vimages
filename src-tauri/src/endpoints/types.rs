824626cf7cb0b9c6aca1741f3298085cb63e21fd
cfsen <103598332+cfsen@users.noreply.github.com>
2025-06-19 10:57:07 +0200

    add: directory metadata type for IPC

--- src-tauri/src/endpoints/types.rs
+++ src-tauri/src/endpoints/types.rs
@@ -1,6 +1,14 @@
 use serde::{Serialize, Deserialize};
 
 #[derive(Serialize, Deserialize)]
+pub struct EntityDirectory {
+    pub path: String,
+    pub parent_path: Option<String>,
+    pub path_hash: String,
+    pub images: Vec<EntityImage>,
+}
+
+#[derive(Serialize, Deserialize)]
 pub struct EntityImage {
     pub full_path: String,
     pub filename: String,
