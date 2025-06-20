84e1f2455491b850cd97d3fc7c0cd656dd190038
cfsen <103598332+cfsen@users.noreply.github.com>
2025-06-20 10:20:15 +0200

    update: endpoint  types
    - add name
    - add optional sub_dirs
    - make images optional
    - rename thumbnail_hash -> img_hash

--- src-tauri/src/endpoints/types.rs
+++ src-tauri/src/endpoints/types.rs
@@ -2,10 +2,12 @@ use serde::{Serialize, Deserialize};
 
 #[derive(Serialize, Deserialize)]
 pub struct EntityDirectory {
+    pub name: String,
     pub path: String,
     pub parent_path: Option<String>,
     pub path_hash: String,
-    pub images: Vec<EntityImage>,
+    pub images: Option<Vec<EntityImage>>,
+    pub sub_dirs: Option<Vec<EntityDirectory>>,
 }
 
 #[derive(Serialize, Deserialize)]
@@ -13,5 +15,5 @@ pub struct EntityImage {
     pub full_path: String,
     pub filename: String,
     pub has_thumbnail: bool,
-    pub thumbnail_hash: String,
+    pub img_hash: String,
 }
