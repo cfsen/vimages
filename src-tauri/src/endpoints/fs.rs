84753b2c48bfc15af8c14020c0d7df9289dcc83a
cfsen <103598332+cfsen@users.noreply.github.com>
2025-06-21 11:03:47 +0200

    update: endpoints
    - update img_cache references, see: 08c2cda
    - remove obsolete endpoints.
    - flag remaining endpoint to discontinue.

--- src-tauri/src/endpoints/fs.rs
+++ src-tauri/src/endpoints/fs.rs
@@ -1,8 +1,5 @@
 use std::{env, fs};
 use std::path::{ Path, PathBuf };
-use base64::engine::general_purpose::STANDARD;
-use base64::Engine;
-use infer;
 
 use crate::img_cache;
 use crate::{get_server_state, server::set_serve_directory};
@@ -44,7 +41,7 @@ pub fn fsx_get_dir(path: &str, rel_path: Option<&str>) -> Result<EntityDirectory
         None => Some("Drives".to_string()),
     };
 
-    let path_hash = img_cache::get_path_hash(&final_path);
+    let path_hash = img_cache::hash::get_path_hash(&final_path);
     let images = fsx_get_images(&final_path);
     let sub_dirs = fs_get_directories(&final_path);
 
@@ -94,7 +91,7 @@ fn fsx_get_images(path: &Path) -> Result<Vec<EntityImage>, String> {
         .map(|entry| -> Result<EntityImage, String> {
             let full_path = entry.path();
             let filename = entry.file_name().to_string_lossy().to_string();
-            let file_hash = img_cache::get_file_hash(&full_path)
+            let file_hash = img_cache::hash::get_file_hash(&full_path)
                 .map_err(|e| format!("Failed to hash: {}: {}", filename, e))?;
 
             // NOTE: debug
@@ -135,7 +132,7 @@ fn fs_get_directories(path: &Path) -> Result<Vec<EntityDirectory>, String> {
             Ok(EntityDirectory {
                 name: dir.file_name().to_string_lossy().to_string(),
                 path: dir_path.to_string_lossy().to_string(),
-                path_hash: img_cache::get_path_hash(&dir_path),
+                path_hash: img_cache::hash::get_path_hash(&dir_path),
                 parent_path: Some(path.to_string_lossy().to_string()),
                 images: None,
                 sub_dirs: None,
@@ -148,160 +145,10 @@ fn fs_get_directories(path: &Path) -> Result<Vec<EntityDirectory>, String> {
     Ok(dirs)
 }
 
-// TODO: refactor below 
-// remove obsolete funcs
-// move thumbnail gen to img_cache
-
+// TODO: still used on launch
 #[tauri::command]
 pub fn fs_get_current_path() -> Result<String, String> {
-    img_cache::get_cache_path();
     env::current_dir()
         .map(|path: PathBuf| path.to_string_lossy().to_string())
         .map_err(|e| e.to_string())
-}
-
-#[tauri::command]
-pub fn fs_get_parent_path(path: String) -> Result<String, String> {
-    let child_path = Path::new(&path);
-    match child_path.parent() {
-        Some(parent) => Ok(parent.to_string_lossy().to_string()),
-        None => Ok(child_path.to_string_lossy().to_string()),
-    }
-}
-
-
-#[tauri::command]
-pub fn fs_list_directory(path: &str) -> Result<Vec<String>, String> {
-    let path = PathBuf::from(path);
-
-    if !path.exists() || !path.is_dir() {
-        return Err("Invalid directory path".into());
-    }
-
-    // update http server dir
-    let server_state = get_server_state();
-    set_serve_directory(server_state, &path)?;
-
-    let mut entries: Vec<String> = vec![];
-    entries.insert(0, "..".to_string());
-    entries.extend( 
-        fs::read_dir(path)
-            .map_err(|e| e.to_string())?
-            .filter_map(Result::ok)
-            .filter(|entry| entry.path().is_dir())
-            .filter_map(|entry| entry.file_name().to_str().map(|s| s.to_string()))
-    );
-
-    entries.sort();
-
-    Ok(entries)
-}
-
-#[tauri::command]
-pub fn fs_get_images(path: &str) -> Result<Vec<String>, String> {
-    let path = PathBuf::from(path);
-
-    if !path.exists() || !path.is_dir() {
-        return Err("Invalid directory path".into());
-    }
-
-    let allowed_exts = ["png", "jpg", "jpeg", "webp", "gif", "svg"];
-
-    let mut entries: Vec<String> = fs::read_dir(path)
-        .map_err(|e| e.to_string())?
-        .filter_map(Result::ok)
-        .filter(|entry| entry.path().is_file())
-        .filter(|entry| { entry
-            .path()
-            .extension()
-            .and_then(|ext| ext.to_str())
-            .map(|ext| allowed_exts.contains(&ext.to_lowercase().as_str()))
-            .unwrap_or(false)
-        })
-        .map(|entry| entry.path().to_string_lossy().into_owned())
-        .collect();
-
-    entries.sort();
-
-    Ok(entries)
-}
-
-#[tauri::command]
-pub async fn fs_get_image_async(path: String) -> Result<Vec<u8>, String> {
-    tauri::async_runtime::spawn_blocking(move || {
-        use std::{fs::File, io::BufReader};
-        use image::{ImageFormat, imageops::FilterType};
-        use image::codecs::jpeg::JpegEncoder;
-
-        let file = File::open(&path).map_err(|e| format!("File open error: {}", e))?;
-        let reader = image::ImageReader::with_format(BufReader::new(file), ImageFormat::Png);
-        let img = reader.decode().map_err(|e| format!("Decode error: {}", e))?;
-
-        let thumbnail = img.resize(400, 400, FilterType::Triangle);
-
-        let mut buffer = Vec::new();
-        let mut encoder = JpegEncoder::new_with_quality(&mut buffer, 60);
-        encoder.encode_image(&thumbnail).map_err(|e| format!("Encode error: {}", e))?;
-
-        Ok(buffer)
-    })
-        .await
-        .map_err(|e| format!("Task join error: {}", e))?
-}
-
-// TODO: unused, remove
-#[tauri::command]
-pub fn fs_get_image(path: &str) -> Result<Vec<u8>, String> {
-    use std::{fs::File, io::BufReader};
-    use image::{ImageFormat, imageops::FilterType};
-    use image::codecs::jpeg::JpegEncoder;
-
-    let file = File::open(path).map_err(|e| format!("File open error: {e}"))?;
-    let reader = image::ImageReader::with_format(BufReader::new(file), ImageFormat::Png);
-    let img = reader.decode().map_err(|e| format!("Decode error: {e}"))?;
-
-    let thumb = img.resize(400, 400, FilterType::Triangle); // much faster than thumbnail()
-
-    let mut buf = Vec::new();
-    let mut encoder = JpegEncoder::new_with_quality(&mut buf, 60); // drop quality for speed
-    encoder.encode_image(&thumb).map_err(|e| format!("Encode error: {e}"))?;
-
-    Ok(buf)
-}
-
-
-#[tauri::command]
-pub fn fs_get_image_data_uri(path: &str) -> Result<String, String> {
-    let start = std::time::Instant::now();
-
-    let path = PathBuf::from(path);
-
-    if !path.exists() || !path.is_file() {
-        return Err("Invalid image path".into());
-    }
-
-    let bytes = fs::read(&path).map_err(|e| e.to_string())?;
-
-    let mime = infer::get(&bytes)
-        .map(|info| info.mime_type())
-        .ok_or("Could not detect file type")?;
-
-    // Only allow certain image MIME types
-    let allowed = [
-        "image/png",
-        "image/jpeg",
-        "image/webp",
-        "image/gif",
-        "image/svg+xml",
-    ];
-
-    if !allowed.contains(&mime) {
-        return Err(format!("Unsupported image type: {}", mime));
-    }
-
-    let encoded = STANDARD.encode(&bytes);
-    let data_uri = format!("data:{};base64,{}", mime, encoded);
-
-    println!("init: fetched image in: {:?}", start.elapsed());
-    Ok(data_uri)
 }
