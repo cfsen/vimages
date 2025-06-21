f8e87fcc03be0a027d43b420c7402602ad7b1741
cfsen <103598332+cfsen@users.noreply.github.com>
2025-06-21 11:04:39 +0200

    update: remove obsolete funcs from lib.rs

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -30,13 +30,7 @@ pub fn run() {
             Ok(())
         })
         .invoke_handler(tauri::generate_handler![
-            fs_get_image_async,
-            fs_get_parent_path,
             fs_get_current_path,
-            fs_list_directory,
-            fs_get_image_data_uri,
-            fs_get_image,
-            fs_get_images,
             fsx_get_dir,
         ])
         .run(tauri::generate_context!())
