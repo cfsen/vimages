cc55e831cc68444b3f1143539a10fb79df4b6c42
cfsen <103598332+cfsen@users.noreply.github.com>
2025-06-22 11:32:04 +0200

    update: img_cache thumbnail generator

--- src-tauri/src/img_cache/img.rs
+++ src-tauri/src/img_cache/img.rs
@@ -1,43 +1,17 @@
-// TODO: rework thumbnail generation
+use std::{ 
+    fs::File, 
+    path::Path
+};
+use image::{self, codecs::webp::WebPEncoder, ImageReader};
 
-pub async fn fs_get_image_async(path: String) -> Result<Vec<u8>, String> {
-    tauri::async_runtime::spawn_blocking(move || {
-        use std::{fs::File, io::BufReader};
-        use image::{ImageFormat, imageops::FilterType};
-        use image::codecs::jpeg::JpegEncoder;
+pub fn create_thumbnail(org_path: &Path, thumb_path: &Path) -> Result<bool, image::ImageError> {
+    let img = ImageReader::open(org_path)?
+        .decode()?;
 
-        let file = File::open(&path).map_err(|e| format!("File open error: {}", e))?;
-        let reader = image::ImageReader::with_format(BufReader::new(file), ImageFormat::Png);
-        let img = reader.decode().map_err(|e| format!("Decode error: {}", e))?;
+    let thumb = img.resize(400, 400, image::imageops::FilterType::Triangle);
+    let output_file = File::create(thumb_path)?;
+    let encoder = WebPEncoder::new_lossless(output_file);
+    thumb.write_with_encoder(encoder)?;
 
-        let thumbnail = img.resize(400, 400, FilterType::Triangle);
-
-        let mut buffer = Vec::new();
-        let mut encoder = JpegEncoder::new_with_quality(&mut buffer, 60);
-        encoder.encode_image(&thumbnail).map_err(|e| format!("Encode error: {}", e))?;
-
-        Ok(buffer)
-    })
-        .await
-        .map_err(|e| format!("Task join error: {}", e))?
+    Ok(true)
 }
-
-pub fn fs_get_image(path: &str) -> Result<Vec<u8>, String> {
-    println!("fs_get_image()");
-    use std::{fs::File, io::BufReader};
-    use image::{ImageFormat, imageops::FilterType};
-    use image::codecs::jpeg::JpegEncoder;
-
-    let file = File::open(path).map_err(|e| format!("File open error: {e}"))?;
-    let reader = image::ImageReader::with_format(BufReader::new(file), ImageFormat::Png);
-    let img = reader.decode().map_err(|e| format!("Decode error: {e}"))?;
-
-    let thumb = img.resize(400, 400, FilterType::Triangle); // much faster than thumbnail()
-
-    let mut buf = Vec::new();
-    let mut encoder = JpegEncoder::new_with_quality(&mut buf, 60); // drop quality for speed
-    encoder.encode_image(&thumb).map_err(|e| format!("Encode error: {e}"))?;
-
-    Ok(buf)
-}
-
