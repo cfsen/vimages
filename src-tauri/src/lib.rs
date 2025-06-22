9eaf8ff0e7ced689ecb54b7c81f2e680a33f94a5
cfsen <103598332+cfsen@users.noreply.github.com>
2025-06-22 11:49:18 +0200

    update: lib.rs queue integration, cleanup
    - add img_cache queue init
    - clean up axum init
    - clean up imports

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,32 +1,43 @@
 mod server;
 mod endpoints;
 mod img_cache;
-use endpoints::fs::*;
+
 use std::{path::PathBuf, sync::{Arc, RwLock, OnceLock}};
 use server::{ServerState, spawn_server};
+use queue::Queue;
+
+use crate::endpoints::fs::{fsx_get_dir, fs_get_current_path};
+use crate::img_cache::queue;
 
 static GLOBAL_SERVER_STATE: OnceLock<ServerState> = OnceLock::new();
+static GLOBAL_QUEUE: OnceLock<Queue> = OnceLock::new();
 
 pub fn get_server_state() -> &'static ServerState {
-    GLOBAL_SERVER_STATE.get().expect("Server state not initialized")
+    GLOBAL_SERVER_STATE.get().expect("Server not initialized")
+}
+
+pub fn get_queue() -> &'static Queue {
+    GLOBAL_QUEUE.get().expect("Queue not initialized")
 }
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
-    let server_state: ServerState = Arc::new(RwLock::new(PathBuf::from(".")));
-
-    // Set the global state
-    GLOBAL_SERVER_STATE.set(server_state.clone()).unwrap();
-
-    let server_state_clone = server_state.clone();
-
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
         .setup(move |_app| {
-            // Start the axum server in a background task using Tauri's async runtime
+            // Init axum
             tauri::async_runtime::spawn(async move {
-                spawn_server(server_state_clone).await;
+                let server_state: ServerState = Arc::new(RwLock::new(PathBuf::from(".")));
+                GLOBAL_SERVER_STATE.set(server_state.clone()).expect("Failed to set server state");
+                spawn_server(server_state).await;
             });
+
+            // Init queue  
+            tauri::async_runtime::spawn(async move {
+                let queue = queue::setup_queue().await;
+                GLOBAL_QUEUE.set(queue).expect("Failed to set global queue");
+            });
+
             Ok(())
         })
         .invoke_handler(tauri::generate_handler![
