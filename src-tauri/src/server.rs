98d36e4fdb85632c198ce7d220bd834d0f721e7f
cfsen <103598332+cfsen@users.noreply.github.com>
2025-06-23 11:05:53 +0200

    add: cache serve to axum

--- src-tauri/src/server.rs
+++ src-tauri/src/server.rs
@@ -1,6 +1,6 @@
 use axum::{
     body::Body,
-    extract::{Query, State},
+    extract::{Path as AxumPath, Query, State},
     http::StatusCode,
     response::{IntoResponse, Response},
     routing::get,
@@ -14,6 +14,8 @@ use std::{
 use tokio::fs;
 use mime_guess::from_path;
 
+use crate::img_cache;
+
 pub type ServerState = Arc<RwLock<PathBuf>>;
 
 pub fn set_serve_directory(server_state: &ServerState, path: &Path) -> Result<(), String> {
@@ -25,7 +27,7 @@ async fn serve_image(
     State(server_state): State<ServerState>,
     Query(params): Query<HashMap<String, String>>,
 ) -> impl IntoResponse {
-   // Get ?file=... parameter
+    // Get ?file=... parameter
     let filename = params.get("file").ok_or(StatusCode::BAD_REQUEST)?;
     let base_dir = server_state.read().unwrap().clone();
     let file_path = base_dir.join(filename);
@@ -50,11 +52,45 @@ async fn serve_image(
         .body(Body::from(file_data))
         .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?;
 
-    Ok(response)}
+    Ok(response)
+}
+
+async fn serve_cache(
+    State(_server_state): State<ServerState>,
+    AxumPath((dir, filename)): AxumPath<(String, String)>,
+) -> impl IntoResponse {
+    let base_dir = img_cache::cache::get_cache_path()
+        .ok_or(StatusCode::BAD_REQUEST)
+        .expect("axum: invalid cache request");
+    let file_path = base_dir.join(&dir).join(format!("{}.webp", &filename));
+
+    // Prevent directory traversal
+    if !file_path.starts_with(&base_dir) {
+        return Err(StatusCode::FORBIDDEN);
+    }
+
+    // Try to read the file
+    let file_data = fs::read(&file_path)
+        .await
+        .map_err(|_| StatusCode::NOT_FOUND)?;
+
+    // Guess the MIME type
+    let mime_guess = from_path(&file_path).first_or_octet_stream();
+    let mime = mime_guess.essence_str();
+
+    // Build response
+    let response = Response::builder()
+        .header("content-type", mime)
+        .body(Body::from(file_data))
+        .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?;
+
+    Ok(response)
+}
 
 async fn start_server(server_state: ServerState) {
     let app = Router::new()
         .route("/image", get(serve_image))
+        .route("/cache/{dir}/{file}", get(serve_cache))
         .with_state(server_state);
 
     let listener = tokio::net::TcpListener::bind("127.0.0.1:8080").await.unwrap();
