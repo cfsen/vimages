939e92fb85cd0de143cf26dab9afa5d73f83799a
cfsen <103598332+cfsen@users.noreply.github.com>
2025-06-23 11:27:29 +0200

    update: show active mode in NavBar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,11 +1,21 @@
 import { useAppState } from "@context/AppContextStore";
+import { Modal } from "@keyboard/KeyboardTypes";
+import styles from "./Navbar.module.css";
 
 function Navbar() {
-	const { currentDir } = useAppState();
+	const currentDir = useAppState(state => state.currentDir);
+	const mode = useAppState(state => state.mode);
 
 	return(
-		<div>
-			{ currentDir }
+		<div className={styles.navbar_container}>
+			<div
+				className={styles.navbar_mode}
+			>
+				{ Modal[mode] } 
+			</div>
+			<div className={styles.navbar_text}>
+				{ currentDir }
+			</div>
 		</div>
 	);
 }
