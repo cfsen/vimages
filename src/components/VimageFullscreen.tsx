3cd80cec20ee6a8802f44c3618992bbd436a8efd
cfsen <103598332+cfsen@users.noreply.github.com>
2025-06-26 12:03:44 +0200

    fix: fullscreen image load performance
    - reduce unnecessary DOM recalculations by avoiding full re-renders when toggling fullscreen image visibility
    - integrate dynamic axum port, see: 690d3d2

--- src/components/VimageFullscreen.tsx
+++ src/components/VimageFullscreen.tsx
@@ -1,19 +1,36 @@
+import { useEffect, useState } from "react";
 import styles from "./Vimage.module.css";
 import { useAppState } from "@context/AppContextStore";
 
 function VimageFullscreen() {
 	const componentEnable = useAppState(state => state.fullscreenImage);
 	const imagePath = useAppState(state => state.fullscreenImagePath);
+	const axum_port = useAppState(state => state.axum_port);
 
-	if (!componentEnable) {
-		return <span></span>;
-	}
+	const [displayImg, setDisplayImg] = useState<boolean>();
+	const [loadedPath, setLoadedPath] = useState<string>();
+
+	useEffect(() => {
+		// display already loaded image (opening, closing then opening the same image)
+		if(loadedPath === imagePath && componentEnable) {
+			setDisplayImg(true);
+		}
+		else {
+			setDisplayImg(false);
+		}
+	}, [imagePath, componentEnable]);
 
 	return (
-		<div className={styles.fullscreenOverlay}>
+		<div 
+			style={{ visibility: componentEnable && displayImg ? 'visible' : 'hidden' }}
+			className={styles.fullscreenOverlay}>
 			<img 
-				src={"http://127.0.0.1:8080/image?file=" + imagePath} 
+				src={`http://127.0.0.1:${axum_port}/image?file=${imagePath}`} 
 				alt="Fullscreen view"
+				onLoad={ () => { 
+					setLoadedPath(imagePath);
+					setDisplayImg(true) 
+				} }
 				className={styles.fullscreenImage}
 			/>
 		</div>
