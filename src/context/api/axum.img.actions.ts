d11e0dfa96c82c65535ec79a9ef5e56e70ba99ec
cfsen <103598332+cfsen@users.noreply.github.com>
2025-07-06 04:45:56 +0200

    add: service layer for axum api calls

---
+++ src/context/api/axum.img.actions.ts
@@ -0,0 +1,16 @@
+import { EntityImage } from "@context/ContextTypes";
+import { useAppState } from "@context/AppContextStore";
+
+export function getImgFromCache(img: EntityImage, path_hash: string | null) {
+	let axum_port = useAppState.getState().axum_port;
+
+	if(axum_port === null) {
+		// TODO: error handling, show to user
+		console.error("internal error, axum port unavailable");
+	}
+
+	if(!img.has_thumbnail || path_hash === null) {
+		return `http://127.0.0.1:${axum_port}/image?file=${img.filename}`;
+	}
+	return `http://127.0.0.1:${axum_port}/cache/${path_hash}/${img.img_hash}`;
+}
