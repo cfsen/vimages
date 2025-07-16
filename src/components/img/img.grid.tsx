24e70f6bc0d901d07317e03b970b91465c5045f5
cfsen <103598332+cfsen@users.noreply.github.com>
2025-07-16 13:41:54 +0200

    chore: image grid internal naming cleanup
    - replace magic numbers with descriptive variables.
    - flag values to hoist to state.

--- src/components/img/img.grid.tsx
+++ src/components/img/img.grid.tsx
@@ -20,6 +20,11 @@ const VimageGrid: React.FC = () => {
 	const { itemsPerRow, setItemsPerRow } = useCommand();
 	const [containerWidth, setContainerWidth] = useState(window.innerWidth);
 
+	// TODO: hoist to store TODO_IMGGRID_HOIST
+	const gap = 7;
+	const border = 1;
+	const window_padding = 0;
+
 	// TODO: Resize handler TODO_MOVE_RESIZE_EVENT
 	useEffect(() => {
 		const handleResize = () => {
@@ -31,10 +36,8 @@ const VimageGrid: React.FC = () => {
 	}, []);
 
 	useEffect(() => {
-		const fullSize = squareBaseSize * scale + 18; // account for padding between elements
-		// TODO: needs to be an option if sidebar+grid will continue being a workspace
-		//const perRow = Math.floor((containerWidth-350) / fullSize); // account for filesystem sidebar
-		const perRow = Math.floor((containerWidth) / fullSize); // account for filesystem sidebar
+		const imgContainerSize = squareBaseSize * scale + 2*gap + 2*border; // account for space between elements
+		const perRow = Math.floor((containerWidth) / imgContainerSize);
 
 		setItemsPerRow(perRow);
 	}, [containerWidth, scale]);
@@ -46,7 +49,8 @@ const VimageGrid: React.FC = () => {
 				style={{
 					display: 'grid',
 					gridTemplateColumns: `repeat(${itemsPerRow}, 0fr)`,
-					gap: '9px',
+					gap: `${gap}px`,
+					padding: `${window_padding}`,
 				}}
 			>
 				{images.map((img, idx) => {
@@ -67,7 +71,7 @@ const VimageGrid: React.FC = () => {
 									alignItems: 'center',
 									justifyContent: 'center',
 									fontSize: '1rem',
-									border: '1px solid rgba(0,0,0,0.3)',
+									border: `${border}px solid rgba(0,0,0,0.3)`,
 								}}
 							>
 								<Vimage id={"vimage" + idx} src={getImgFromCache(img, path_hash) } />
