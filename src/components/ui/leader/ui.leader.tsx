abac55b9de7440abe4f2857e2339d31ad2fa1358
cfsen <103598332+cfsen@users.noreply.github.com>
2025-07-31 05:09:12 +0200

    update: dynamic leader overlay population
    Add dynamic command population for the leader overlay UI. Valid commands are fetched from current keybindings.

--- src/components/ui/leader/ui.leader.tsx
+++ src/components/ui/leader/ui.leader.tsx
@@ -1,18 +1,32 @@
 import { useAppState } from "@/context/app/app.context.store";
 import styles from "./ui.leader.module.css";
 import { Modal } from "@/context/key/key.types";
+import { useState } from "react";
+import { getCurrentKeybinds } from "@/context/key/key.module";
+import { Command } from "@/context/key/key.command";
 
 function Leader() {
 	const mode = useAppState(s => s.mode);
-
-	// TODO: dynamic populate
+	const [binds, setBinds] = useState(getCurrentKeybinds());
 
 	if(mode !== Modal.Leader) return;
 	return(
 		<div className={styles.uiLeaderOverlay}>
 			<div className={styles.uiLeaderGrid}>
-				<div>[<b>w</b>] Next workspace</div>
-				<div>[<b>W</b>] Prev workspace</div>
+				{Array.from(binds.keyMap)
+					.filter(([command]) => command.startsWith(binds.commandMap.get(Command.Leader)!))
+					.filter(([command]) => command !== binds.commandMap.get(Command.Leader))
+					.map(([command, key]) => (
+						<div key={`leaderOverlay_${command}`}>
+							[<b>{
+								command
+								.slice(binds.commandMap.get(Command.Leader)!.length)
+								.replace(binds.commandMap.get(Command.Leader)!, "leader")
+							}</b>] {
+								Command[key]
+							}
+						</div>
+					))}
 			</div>
 		</div>
 	);
