f5456974c3100ebf51956288e9e31daf22181bb1
cfsen <103598332+cfsen@users.noreply.github.com>
2025-08-14 10:09:04 +0200

    update: contextual scrollToActive targeting
    scrollToActive now uses `itemType` from `NavWrapper` to determine which method should be used for scroll targeting.

--- src/context/nav/nav.provider.actions.ts
+++ src/context/nav/nav.provider.actions.ts
@@ -1,6 +1,6 @@
 import { StoreApi } from "zustand";
 import { INavigationState } from "@nav/nav.provider.store";
-import { NavWrapperUIState } from "@nav/nav.types";
+import { NavigationItem, NavWrapperItemType, NavWrapperUIState } from "@nav/nav.types";
 import { IAppState } from "../app/app.context.store";
 
 export function activeNavWrapper(navProviderStore: StoreApi<INavigationState>, activeProvider: boolean, id: string): NavWrapperUIState {
@@ -16,19 +16,60 @@ export function activeNavWrapper(navProviderStore: StoreApi<INavigationState>, a
 
 export function scrollToActive(navProvider: StoreApi<INavigationState>){
 	const cursorElement = navProvider.getState().navItems
-	.filter((a) => a.id === navProvider.getState().navItemActive);
+	.find((a) => a.id === navProvider.getState().navItemActive);
 
-	if(cursorElement.length !== 1 || cursorElement[0].ref.current === null) return;
+	if(cursorElement === undefined) {
+		console.error("scrollToActive was called on undefined element.");
+		return;
+	}
+	if(cursorElement.ref.current === null) { 
+		console.error("scrollToActive was called on null ref.");
+		return;
+	}
 
-	const rect = cursorElement[0].ref.current.getBoundingClientRect();
-	// TODO: get rid of magic number tuned layout
-	const targetY = window.scrollY + rect.top - (window.innerHeight / 2) - (rect.height * 4.5);
+	let targetY = null;
+
+	switch(cursorElement.itemType){
+		case NavWrapperItemType.Image:
+			targetY = getScrollPositionImageGrid(cursorElement);
+			break;
+		case NavWrapperItemType.FileBrowser:
+			targetY = getScrollPositionFileBrowser(cursorElement);
+			break;
+	};
+
+	if(targetY === null) {
+		console.error("scollToActive failed to calculate targetY.");
+		return;
+	}
 
 	scrollTo({
 		left: 0,
 		top: targetY,
 		behavior: "instant",
 	});
+}
+
+function getScrollPositionFileBrowser(navElement: NavigationItem){
+	const rect = navElement.ref.current?.getBoundingClientRect() ?? null;
+	if(rect === null){
+		console.error("navElement was null, failed to scroll.");
+		return null;
+	}
+
+	const targetY = window.scrollY + rect.top - (rect.height / 2) - (window.innerHeight / 2);
+	return targetY;
+}
+
+function getScrollPositionImageGrid(navElement: NavigationItem){
+	const imageElement = navElement.ref.current?.parentElement?.getBoundingClientRect() ?? null;
+	if(imageElement === null) {
+		console.warn("imageElement was null, falling back to scrollToActive");
+		return getScrollPositionFileBrowser(navElement);
+	}
+
+	const targetY = window.scrollY + imageElement.top - (4*imageElement.height) - (window.innerHeight / 2);
+	return targetY;
 }
 
 export function scrollToActive_Delayed(navProvider: StoreApi<INavigationState>, appStore: StoreApi<IAppState>){
