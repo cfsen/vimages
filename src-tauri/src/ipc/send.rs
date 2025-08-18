fad1ceecad4b9d4bc24df895c716fd25b5d64914
cfsen <103598332+cfsen@users.noreply.github.com>
2025-08-18 15:00:17 +0200

    update: reduce debug output from ipc senders

--- src-tauri/src/ipc/send.rs
+++ src-tauri/src/ipc/send.rs
@@ -5,13 +5,11 @@ use tauri::Emitter;
 use crate::{get_app_handle, ipc::types::{IpcMsgInfoWindow, IpcQueueState}};
 
 pub fn info_window_msg(message: &str) -> bool {
-    debug!("IPC: emit: info_window_msg");
-
     let res = get_app_handle().emit_to("global-handler", "msg-info-window", IpcMsgInfoWindow { message });
 
     match res {
         Ok(_) => {
-            debug!("IPC: info_window_msg emitted successfully");
+            debug!("IPC: info_window_msg emitted");
             true
         }
         Err(e) => {
@@ -21,19 +19,17 @@ pub fn info_window_msg(message: &str) -> bool {
     }
 }
 pub fn queue_status(path: &Path, redraw: &bool, queue_empty: &bool) -> bool {
-    debug!("IPC: emit: queue_status");
-
     let path = path.to_str().unwrap();
 
     let res = get_app_handle().emit_to("global-handler", "msg-queue-status", IpcQueueState { path, redraw, queue_empty });
 
     match res {
         Ok(_) => {
-            debug!("IPC: info_window_msg emitted successfully");
+            debug!("IPC: queue_status emitted");
             true
         }
         Err(e) => {
-            debug!("IPC: info_window_msg failed: {e}");
+            debug!("IPC: queue_status failed: {e}");
             false
         }
     }
