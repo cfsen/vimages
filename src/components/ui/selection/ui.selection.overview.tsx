1b4b1af376d3d57defdf10df6ce807c76343d126
cfsen <103598332+cfsen@users.noreply.github.com>
2025-08-20 11:46:49 +0200

    update: selection widget visual tweaks
    - show selection count.
    - show path to current directory.
    - trim filenames to fit ui.

--- src/components/ui/selection/ui.selection.overview.tsx
+++ src/components/ui/selection/ui.selection.overview.tsx
@@ -1,13 +1,12 @@
-import { useAppState} from "@app/app.context.store";
-
-import { getActiveNavigationProvider } from "@/context/app/app.context.actions";
 import { useEffect, useState } from "react";
 
+import { useAppState} from "@app/app.context.store";
+
 import styles from "./ui.selection.module.css"
-import { NavigationHandle, UIComponent } from "@/context/context.types";
 
 function SelectionOverview() {
 	const activeSelection = useAppState(s => s.activeSelection);
+	const currentDir = useAppState(s => s.currentDir);
 	const [selection, setSelection] = useState<string[]>([]);
 
 	useEffect(() => {
@@ -28,14 +27,21 @@ function SelectionOverview() {
 
 	return(
 		<div className={styles.selectionOverlay}>
-			Selection overview
+			<div className={styles.selectionHeadline}>Selecting {selection.length} items</div>
+			<div className={styles.selectionDirectory}>{currentDir}</div>
 			{selection.map((k, i) => (
 				<div key={`selectionOverviewIndex_${i}`}>
-					{k}
+					{trimStrLength(k)}
 				</div>
 			))}
 		</div>
 	);
+}
+
+function trimStrLength(str: string){
+	if(str.length > 45)
+		return str.substring(0, 31) + " (…)";
+	return str;
 }
 
 export default SelectionOverview;
