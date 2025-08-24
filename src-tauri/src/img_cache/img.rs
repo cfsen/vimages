cf64ca3179f6110493e5e3dbe258ce4a0024ac8d
cfsen <103598332+cfsen@users.noreply.github.com>
2025-08-24 13:11:31 +0200

    update: image resize from Triangle to Nearest
    Increases thumbnail generation speed. Should be exposed as a setting to provide users agency over performance and quality.

--- src-tauri/src/img_cache/img.rs
+++ src-tauri/src/img_cache/img.rs
@@ -4,7 +4,7 @@ use std::{fs::File, path::Path};
 pub fn create_thumbnail(org_path: &Path, thumb_path: &Path) -> Result<(), image::ImageError> {
     let img = ImageReader::open(org_path)?.decode()?;
 
-    let thumb = img.resize(400, 400, image::imageops::FilterType::Triangle);
+    let thumb = img.resize(400, 400, image::imageops::FilterType::Nearest);
     let output_file = File::create(thumb_path)?;
     let encoder = WebPEncoder::new_lossless(output_file);
 
