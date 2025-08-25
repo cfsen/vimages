bae6833594337a4dfa9824170d3e2fab66cc1fb5
cfsen <103598332+cfsen@users.noreply.github.com>
2025-08-25 11:59:07 +0200

    refactor: front-end queue IPC event handler
    - update to handle new queue IPC message data structure.
    - add support for `ImageComplete` opcode, updating store thumbnail state for image with matching hash.

--- src/context/app/app.event.listeners.ts
+++ src/context/app/app.event.listeners.ts
@@ -2,45 +2,26 @@ import { StoreApi } from "zustand";
 import { Event as TauriEvent} from "@tauri-apps/api/event";
 
 import { IAppState } from "@app/app.context.store";
-import { addInfoMessage, getDirectorySkipLock, IPC_PendingRemoves } from "@app/app.context.actions";
+import { addInfoMessage, updateImageThumbnailState } from "@app/app.context.actions";
 
-import { IPC_MsgInfoWindow, IPC_QueueStatus } from "./app.event.types";
+import { IPC_MsgInfoWindow, IPC_QueueOpCode, IPC_QueueStatus } from "./app.event.types";
 
 export const eventHandleMsgInfoWindow = (event: TauriEvent<IPC_MsgInfoWindow>, useAppState: StoreApi<IAppState>) => {
 	addInfoMessage(useAppState, event.payload.message);
 };
 
 export const eventHandleQueueState = (event: TauriEvent<IPC_QueueStatus>, store: StoreApi<IAppState>) => {
-	// TODO: REFACTOR: REFACTOR_IMAGE_PIPELINE 
-	// currently only receives redraw messages when lock should be released.
-	//  - implement proper opcodes later.
-	//  - backend queue pipeline needs to be improved to discern individual directories.
+	// TODO: handle other messages
 
-	if(event.payload.queueEmpty) {
-		console.log("Queue is empty, clear all locks.");
-		IPC_PendingRemoves.clear();
+	let msg = event.payload as IPC_QueueStatus;
 
-		// Redraw
-		if(event.payload.path === store.getState().currentDir)
-			getDirectorySkipLock(store, ".");
-		return;
+	if(msg.opcode === IPC_QueueOpCode.ImageComplete) {
+		if(msg.imgHash === null) {
+			console.error("Incomplete message from backend. OpCode=ImageComplete, but imgHash was null.");
+			return;
+		}
+		updateImageThumbnailState(store, msg.imgHash, true);
 	}
 
-	if(!IPC_PendingRemoves.has(event.payload.path)){
-		console.warn("eventHandleQueueState was called, but no lock exists for path: " + event.payload.path);
-		return;
-	}
-
-	IPC_PendingRemoves.delete(event.payload.path);
-
-	if(event.payload.path !== store.getState().currentDir){
-		console.log("Queue message redraw call ignored, path not currentDir.");
-		console.log("path=" + event.payload.path + " || currentDir=" + store.getState().currentDir);
-		return;
-	}
-
-	if(event.payload.redraw) {
-		getDirectorySkipLock(store, ".");
-		return;
-	}
+	return;
 }
