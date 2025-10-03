5bdbe75a7304405150742435138f13cbe1f03b26
cfsen <103598332+cfsen@users.noreply.github.com>
2025-10-03 07:15:59 +0200

    chore: docs for app.event.types.ts

--- src/context/app/app.event.types.ts
+++ src/context/app/app.event.types.ts
@@ -1,15 +1,33 @@
+//
+// Types used in IPC calls
+//
+
+// Messages passed to log widget
+// Event handler: eventHandleMsgInfoWindow (src/context/app/app.event.listeners.ts)
+// See: IpcMsgInfoWindow (src-tauri/src/ipc/types.rs)
 export type IPC_MsgInfoWindow = {
 	message: string,
 };
+
+// Queue status updates to event listener
+// Event handler: eventHandleQueueState (src/context/app/app.event.listeners.ts)
+// Rust type: IpcQueueState (src-tauri/src/ipc/types.rs)
 export type IPC_QueueStatus = {
 	opcode: IPC_QueueOpCode,
 	imgHash: string | null,
 	pathHash: string | null,
 };
+
+// Encapsulates multiple messages passed to log widget
+// Event handler: eventHandleQueueStringArray (src/context/app/app.event.listeners.ts)
+// Rust type: IpcDataStringArray (src-tauri/src/ipc/types.rs)
 export type IPC_DataStringArray = {
 	opcode: IPC_QueueOpCode,
 	data: string[],
 };
+
+// Opcodes for IPC calls
+// Rust enum: IpcQueueOpCode (src-tauri/src/ipc/types.rs)
 export enum IPC_QueueOpCode {
 	ImageComplete = "ImageComplete",
 	ImageFailed = "ImageFailed",
