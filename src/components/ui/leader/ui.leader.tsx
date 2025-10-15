d809e28206d0241c0fc28be7d4d4b9b430c08f0b
cfsen <103598332+cfsen@users.noreply.github.com>
2025-10-15 09:06:10 +0200

    chore: minor refactor of ui.leader.tsx

--- src/components/ui/leader/ui.leader.tsx
+++ src/components/ui/leader/ui.leader.tsx
@@ -1,7 +1,6 @@
-import { useState } from "react";
 import { useAppState } from "@app/app.context.store";
 
-import { Modal } from "@key/key.types";
+import { Keybinds, Modal } from "@key/key.types";
 import { getCurrentKeybinds } from "@key/key.module";
 import { Command } from "@key/key.command";
 
@@ -9,29 +8,36 @@ import styles from "./ui.leader.module.css";
 
 function Leader() {
 	const mode = useAppState(s => s.mode);
-	const [binds, setBinds] = useState(getCurrentKeybinds());
+	const binds = getCurrentKeybinds();
+	const leaderCommands = GetLeaderCommands(binds);
 
 	if(mode !== Modal.Leader) return;
+
 	return(
 		<div className={styles.uiLeaderOverlay}>
 			<div className={styles.uiLeaderGrid}>
-				{Array.from(binds.keyMap)
-					.filter(([command]) => command.startsWith(binds.commandMap.get(Command.Leader)!))
-					.filter(([command]) => command !== binds.commandMap.get(Command.Leader))
-					.map(([command, key]) => (
-						<div key={`leaderOverlay_${command}`}>
-							[<b>{
-								command
-								.slice(binds.commandMap.get(Command.Leader)!.length)
-								.replace(binds.commandMap.get(Command.Leader)!, "leader")
-							}</b>] {
-								Command[key]
-							}
-						</div>
-					))}
+				{leaderCommands.map(([command, key]) => (
+					<div key={`leaderOverlay_${command}`}>
+						[<b>{ GetLeaderKeyLabel(command, binds) }</b>] { Command[key] }
+					</div>
+				))}
 			</div>
 		</div>
 	);
+}
+
+// TODO: move out from component
+function GetLeaderCommands(binds: Keybinds) {
+	return Array.from(binds.keyMap)
+		.filter(([command]) => command.startsWith(binds.commandMap.get(Command.Leader)!))
+		.filter(([command]) => command !== binds.commandMap.get(Command.Leader));
+}
+
+// TODO: move out from component
+function GetLeaderKeyLabel(command: string, binds: Keybinds): string{
+	return command
+		.slice(binds.commandMap.get(Command.Leader)!.length)
+		.replace(binds.commandMap.get(Command.Leader)!, "leader")
 }
 
 export default Leader;
