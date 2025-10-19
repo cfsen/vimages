d3be47e1167d84dfae7c56d4c17fceffa08a8d08
cfsen <103598332+cfsen@users.noreply.github.com>
2025-10-19 06:15:28 +0200

    chore: docs and org for app.event.listeners.ts

--- src/context/app/app.event.listeners.ts
+++ src/context/app/app.event.listeners.ts
@@ -6,28 +6,30 @@ import { addInfoMessage, addInfoMessageArray, updateImageThumbnailStateBatch } f
 
 import { IPC_DataStringArray, IPC_MsgInfoWindow, IPC_QueueOpCode, IPC_QueueStatus } from "./app.event.types";
 
-export const eventHandleMsgInfoWindow = (event: TauriEvent<IPC_MsgInfoWindow>, useAppState: StoreApi<IAppState>) => {
-	addInfoMessage(useAppState, event.payload.message);
-};
+//
+// double buffer thumbnail updates
+//
 
 // NOTE: image pipeline can output images faster than react can complete state updates, causing UI lag.
 // batching updates to images to partially mitigate this.
 // TODO: review DS and explore other options for potential improvements
 
-// double buffer thumbnail updates
+// buffer tuning
 const flushTimeout = 200; // [ms] to wait without pushes to buffer before clearing
 const thumbBufFlushThreshold = 100; // capacity of each buffer
 
-let thumbTimerFlush: number | null = null;
+// buffer globals
+let thumbTimerFlush: number | null = null; // timer for flush
 
-let thumbBuf: Set<string>[] = [];
+let thumbBuf: Set<string>[] = []; // buffers 0 and 1
 thumbBuf.push(new Set<string>());
 thumbBuf.push(new Set<string>());
-let thumbBufLength: number[] = [0,0];
+let thumbBufLength: number[] = [0,0]; // buffer length
 
-let thumbActiveBuf: number = 0;
-let thumbInactiveBuf: number = 1;
+let thumbActiveBuf: number = 0; // buffer which should receive items [0/1]
+let thumbInactiveBuf: number = 1; // inactive buffer [0/1]
 
+// push a completed thumbnail identified by hash `imgHash` to buffer
 function bufferThumbnailUpdate(imgHash: string): Set<string> | null {
 	thumbBuf[thumbActiveBuf].add(imgHash);
 	thumbBufLength[thumbActiveBuf] += 1;
@@ -44,6 +46,7 @@ function bufferThumbnailUpdate(imgHash: string): Set<string> | null {
 	return null;
 }
 
+// flush active buffer, returning any remaining items and resetting both buffers
 function bufferFlush(): Set<string> | null {
 	let flush;
 	if(thumbBufLength[thumbActiveBuf] > 0) {
@@ -60,6 +63,7 @@ function bufferFlush(): Set<string> | null {
 	return flush;	
 }
 
+// handle messages with queue updates
 export const eventHandleQueueState = (event: TauriEvent<IPC_QueueStatus>, store: StoreApi<IAppState>) => {
 	let msg = event.payload as IPC_QueueStatus;
 
@@ -98,6 +102,17 @@ export const eventHandleQueueState = (event: TauriEvent<IPC_QueueStatus>, store:
 
 	return;
 }
+
+//
+// info widget ipc handlers
+//
+
+// handle messages with string payloads
+export const eventHandleMsgInfoWindow = (event: TauriEvent<IPC_MsgInfoWindow>, useAppState: StoreApi<IAppState>) => {
+	addInfoMessage(useAppState, event.payload.message);
+};
+
+// handle messages with string array payloads
 export const eventHandleQueueStringArray = (event: TauriEvent<IPC_DataStringArray>, store: StoreApi<IAppState>) => {
 	switch(event.payload.opcode){
 		case IPC_QueueOpCode.StatusQueue:
@@ -108,4 +123,4 @@ export const eventHandleQueueStringArray = (event: TauriEvent<IPC_DataStringArra
 			break;
 	}
 	addInfoMessageArray(store, event.payload.data);
-}
+};
