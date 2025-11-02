534016d8c5462933cbd7f3a87118ec0182fe51e8
cfsen <103598332+cfsen@users.noreply.github.com>
2025-11-02 10:24:03 +0100

    update: help overlay first draft for command mode
    Add basic layout and short description of available commands to `:help` overlay.

--- src/components/ui/help/ui.help.overlay.tsx
+++ src/components/ui/help/ui.help.overlay.tsx
@@ -12,12 +12,84 @@ function HelpOverlay() {
 	return (
 		<div className={styles.fullscreenHelpOverlay}>
 			<h2>vimages</h2>
-			<div>
-				<b>esc</b> to close
-			</div>
 			<p>
-				WIP
+				<b>esc</b> to close this overlay.
 			</p>
+			{HelpCommandMode()}
+		</div>
+	);
+}
+
+function HelpCommandMode(){
+	return(
+		<div className={styles.uiHelpNodeContainer}>
+			<h3>Commands </h3>
+
+			<div className={styles.uiHelpNode}>
+				<div className={styles.uiHelpKeyBox}>:</div>
+				<div className={styles.uiHelpCommandDescription}>Command mode.</div>
+			</div>
+
+			<div className={styles.uiHelpNode}>
+				<div className={styles.uiHelpKeyBox}>:q</div>
+				<div className={styles.uiHelpCommandDescription}>Exit vimages.</div>
+			</div>
+
+			<div className={styles.uiHelpNode}>
+				<div className={styles.uiHelpKeyBox}>:wq</div>
+				<div className={styles.uiHelpCommandDescription}>Save config and exit.</div>
+			</div>
+
+			<div className={styles.uiHelpNode}>
+				<div className={styles.uiHelpKeyBox}>:sc</div>
+				<div className={styles.uiHelpCommandDescription}>Save config.</div>
+			</div>
+
+			<div className={styles.uiHelpNode}>
+				<div className={styles.uiHelpKeyBox}>:help</div>
+				<div className={styles.uiHelpCommandDescription}>Show this overlay.</div>
+			</div>
+
+			<div className={styles.uiHelpNode}>
+				<div className={styles.uiHelpKeyBox}>:set</div>
+				<div className={styles.uiHelpCommandSub}>imgscale, errorlv, titlebar, infowindow, scrollDelay, genericError</div>
+				<div className={styles.uiHelpCommandDescriptionBelow}>Set application preferences.</div>
+			</div>
+
+			<div className={styles.uiHelpNode}>
+				<div className={styles.uiHelpKeyBox}>:get</div>
+				<div className={styles.uiHelpCommandSub}>version, queue</div>
+				<div className={styles.uiHelpCommandDescriptionBelow}>Print variables.</div>
+			</div>
+
+			<div className={styles.uiHelpNode}>
+				<div className={styles.uiHelpKeyBox}>:fullscreen</div>
+				<div className={styles.uiHelpCommandSub}>invertCursor, moveStep, rotateStep, zoomStep, remapCursor</div>
+				<div className={styles.uiHelpCommandDescriptionBelow}>Set fullscreen image viewing preferences.</div>
+			</div>
+
+			<div className={styles.uiHelpNode}>
+				<div className={styles.uiHelpKeyBox}>:cache</div>
+				<div className={styles.uiHelpCommandSub}>info, clean, path</div>
+				<div className={styles.uiHelpCommandDescriptionBelow}>Cache info and cleaning.</div>
+			</div>
+
+			<div className={styles.uiHelpNode}>
+				<div className={styles.uiHelpKeyBox}>:config</div>
+				<div className={styles.uiHelpCommandDescription}>Open config viewer widget.</div>
+			</div>
+
+			<div className={styles.uiHelpNode}>
+				<div className={styles.uiHelpKeyBox}>:cd</div>
+				<div className={styles.uiHelpCommandSub}>[path]</div>
+				<div className={styles.uiHelpCommandDescriptionBelow}>Open directory at [path].</div>
+			</div>
+
+			<div className={styles.uiHelpNode}>
+				<div className={styles.uiHelpKeyBox}>:queue</div>
+				<div className={styles.uiHelpCommandSub}>blacklist, bl, status</div>
+				<div className={styles.uiHelpCommandDescriptionBelow}>Print information about queued jobs.</div>
+			</div>
 		</div>
 	);
 }
