869664e1ebad41c63ab3a1053922e1fa5c9d5518
cfsen <103598332+cfsen@users.noreply.github.com>
2026-01-02 01:46:48 +0100

    update: sync insert mode input to zustand

--- src/context/app/handler/mode.insert.ts
+++ src/context/app/handler/mode.insert.ts
@@ -1,5 +1,28 @@
+import { Command } from "@key/key.command";
+import { useAppState } from "../app.context.store";
 import { resultModeCommand } from "@/context/key/key.module.handler.cmd";
+import { InsertModeCompAction } from "@/context/context.types";
+
 export function InsertModeHandler(resultInsert: resultModeCommand){
-	console.log("InsertModeHandler", resultInsert);
+	const {
+		insertBuffer,
+		setInsertBuffer,
+		insertCompAction,
+	} = useAppState.getState();
+
+	// NOTE: clearing the buffer on escape is handled by key module,
+	// which will send an empty string when escape is pressed.
+	setInsertBuffer(resultInsert.sequence);
+
+	// Send buffer on return
+	if(resultInsert.cmd !== Command.Return) {
+		return;
+	}
+
+	if(insertCompAction == InsertModeCompAction.EntityBatchRename) {
+		// TODO: impl call to backend for renaming
+		console.debug("Call backend to execute rename.");
+		console.debug("Files: " + insertBuffer);
+	}
 }
 
