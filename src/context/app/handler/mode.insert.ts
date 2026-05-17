a4d506b82358da9fe662280e4c5f615ab11c3ef6
cfsen <103598332+cfsen@users.noreply.github.com>
2026-05-17 06:06:34 +0200

    chore: remove unused state hook

--- src/context/app/handler/mode.insert.ts
+++ src/context/app/handler/mode.insert.ts
@@ -7,7 +7,6 @@ import { invoke } from "@tauri-apps/api/core";
 
 export function InsertModeHandler(resultInsert: resultModeCommand){
 	const {
-		insertBuffer,
 		setInsertBuffer,
 		insertCompAction,
 		setInsertCompAction,
